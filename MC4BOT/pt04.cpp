19a3cc40faacc4cecddf475f927cf43f08a8ec3b
Kyle Westhaus <kwesthaus@users.noreply.github.com>
2019-03-22 20:09:30 -0400

    Abstracts control for wait time between actions

--- MC4BOT/pt04.cpp
+++ MC4BOT/pt04.cpp
@@ -24,66 +24,74 @@ void pt04() {
 
         //Move robot up slightly
         driveForTime(2.1, MotorPercentWeak, DirectionForward);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //Turn the robot CW to face the DDR button
         turnForTime(1.7, MotorPercentWeak, DirectionClockwise);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //Move robot towards DDR
         driveForTime(2.9, MotorPercentMedium, DirectionForward);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //Angle robot towards DDR
         turnForTime(3.2, MotorPercentWeak, DirectionClockwise);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //Press button
         driveForTime(2.0, MotorPercentMedium, DirectionForward);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //Back away from DDR
         driveForTime(1.0, MotorPercentMedium, DirectionBackward);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //Rotate robot 180 degrees to face ramp
         turnForTime(7.0, MotorPercentWeak, DirectionClockwise);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //USE RPS TO CHECK ANGLE
         rpsCheckHeading(95.0);
+        Sleep(ACTION_SEP_PAUSE);
+
 //--------------------------------------------------------------------------------------------------------------------------------------------------//
         //Foosball
         //Move robot up ramp to foosball
         driveForTime(4.0, MotorPercentStrong, DirectionForward);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //USE RPS TO CHECK ANGLE after up ramp
         rpsCheckHeading(95.0);
+        Sleep(ACTION_SEP_PAUSE);
+
 
         //Continue moving robot forward to foosball
         driveForTime(3.0, MotorPercentStrong, DirectionForward);
+        Sleep(ACTION_SEP_PAUSE);
+
 
         //MIGHT NEED TO ROTATE, MOVE FORWARD, ROTATE
 
         //Rotate robot 90 counterclockwise
         turnForTime(3.1, MotorPercentWeak, DirectionCounterClockwise);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //Move forward slightly so claw can reach foosball counter
         driveForTime(1.0, MotorPercentMedium, DirectionForward);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //Drop arm
         servoClaw.SetDegree(SERVO_CLAW_POS_ACTIVE);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //Move robot forward
         driveForTime(3.0, MotorPercentWeak, DirectionForward);
-        Sleep(1.0);
+        Sleep(ACTION_SEP_PAUSE);
 
         //Lift arm
         servoClaw.SetDegree(SERVO_CLAW_POS_NEUTRAL);
+        Sleep(ACTION_SEP_PAUSE);
+
 
     return;
 }
